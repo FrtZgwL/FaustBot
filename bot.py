6f604d04a2d2f177f4f9335859d53e6944c5ab0e
Cedric Tonye Djon <Cedric@s-8d3a236b.on.site.uni-stuttgart.de>
2018-09-06 03:40:43 +0200

    Tür Funktion

--- bot.py
+++ bot.py
@@ -8,6 +8,9 @@ import time
 import json
 from telepot.loop import MessageLoop
 
+# Temporär?
+import pprint
+
 
 ###############################################
 # ---   Globale Variablen & Konstanten    --- #
@@ -15,61 +18,195 @@ from telepot.loop import MessageLoop
 
 # --- Konstanten --- #
 
-NORMAL = 0
-PASSWORT = 1
-SCHLÜSSEL = 2
-GRUPPEN = 3
+M_NORMAL = 0
+M_PASSWORT = 1
+M_SCHLÜSSEL = 2
+M_GRUPPEN = 3
+
+SUPPORTTEAM = "Cedric und Maurice"
+PASSWORT = ""
+TOKEN = ""
 
 # --- Variablen --- #
 
-display_message = {}
 menüs = {}
+users = {}
+data = {}
+
+# --- Globale Variablen --- #
+
+display_message = {}
+pp = pprint.PrettyPrinter(indent=2)
 
 
-#################################
-# ---   json Datein laden   --- #
-#################################
+###########################################
+# ---   json Datein barbeiten/laden   --- #
+###########################################
 
 with open("Daten/menüs.json", "r") as f:
     menüs = json.load(f)
 
+with open("Daten/users.json", "r") as f:
+    users = json.load(f)
+
+with open("Daten/data.json", "r") as f:
+    data = json.load(f)
+
+with open("Daten/config.json", "r") as f:
+    config = json.load(f)
+    if config["token"] == "":
+        sys.exit("No token defined. Define it in a file called token.txt.")
+    if config["passwort"] == "":
+        print("WARNING: Empty Password for registering to use the bot." +
+              " It could be dangerous, because anybody could use this bot" +
+              " and forward messages to the channels associated to it")
+    SUPPORTTEAM = config["supportteam"]
+    PASSWORT = config["passwort"]
+    TOKEN = config["token"]
+
+def save(pfad, obj):
+    with open(pfad, "w") as f:
+        f.write(json.dumps(obj, indent=2, sort_keys=True))
+
+
+# users aus datei laden
+
+# SUPPORTTEAM aus config laden
+# TOKEN aus config laden
+# PASSWORT und weitere Passwörter
 
 #########################
 # ---   Funktionen  --- #
 #########################
 
+
+def build_name(msg):
+    return msg["from"]["first_name"] + (" " + msg["chat"]["last_name"] if "last_name" in msg["chat"] else "")
+
+
 def handle(msg):
     global display_message
+    global users
+    global data
 
     flavor = telepot.flavor(msg)
 
     if flavor == "chat":
-        # Variablen setzen
         chat_id = msg["chat"]["id"]
         txt = msg["text"]
 
+
+        ##################################
+        # ---   Telegram Befehle    ---- #
+        ##################################
+
         if "/start" == txt[:6]:
-            # Wenn noch nicht Nutzer, dann (hinzufügen, in Passwort-Modus und) checken ob Passwort korrekt. Wenn schon Nutzer, Hauptmenü anzeigen
-            display_message = bot.sendMessage(chat_id, "PLATZHALTER START", reply_markup=json.dumps(menüs["haupt"]))
-        elif "/help" == txt[:5]:
-            # Hilfe-Text anzeigen
-            bot.sendMessage(chat_id, "HIER IST HILFE")
+            if msg["chat"]["type"] != "private":
+                bot.sendMessage(chat_id, "Dieser Befehl kann nur in privaten Chats verwendet werden.")
+            else:
+                # Wenn noch nicht Nutzer, dann (hinzufügen, in Passwort-Modus und) checken ob Passwort korrekt. Wenn schon Nutzer, Hauptmenü anzeigen
+                if str(chat_id) not in users:
+                    users[str(chat_id)] = {
+                    "name":build_name(msg),
+                    "modus":M_PASSWORT,
+                    "current_group":"",
+                    "schulden":0.0,
+                    "is_allowed":False,
+                    "is_finanzen":False,
+                    "is_einkauf":False,
+                    "is_admin":False
+                    }
+                    save("Daten/users.json", users)
+
+                    bot.sendMessage(chat_id, "Hi, " + build_name(msg) + "!\nGibt bitte das Passwort ein, um den Zugriff zu erhalten. Falls du das Passwort nicht weiß, schreib eine Nachricht an " + SUPPORTTEAM + ".")
+
+                elif users[str(chat_id)]["is_allowed"]:
+                    display_message = bot.sendMessage(chat_id, "Hauptmenü", reply_markup=json.dumps(menüs["haupt"]))
+
+                else:
+                    display_message = bot.sendMessage(chat_id, "Hey, du bist leider noch nicht als Nutzer hinzugefügt. Gib bitte erst das richtige Passwort ein. Bei Problemen kanns du dich immer an " + SUPPORTTEAM + " wenden")
+
+        elif "/help" == txt[:5] or "/?" == txt[:2]:
+            pp.pprint(users)
+
+            if str(chat_id) in users:
+                if users[str(chat_id)]["is_allowed"]:
+                    bot.sendMessage(chat_id, "HIER IST HILFE")
+                else:
+                    bot.sendMessage(chat_id, "Nur registrierte Nutzer können diese Funktion nutzen.")
+            else:
+                bot.sendMessage(chat_id, "Nur registrierte Nutzer können diese Funktion nutzen.")
+
+
+        #####################
+        # ---   Modi    --- #
+        #####################
+        else:
+            modus = users[str(chat_id)]["modus"]
+
+            if modus == M_PASSWORT:
+                if txt == PASSWORT:
+                    users[str(chat_id)]["is_allowed"] = True
+                    users[str(chat_id)]["modus"] = M_NORMAL
+                    save("Daten/users.json", users)
+
+                    bot.sendMessage(chat_id, "Du wurdest erfolgreich als Nutzer hinzugefügt! Viel Spaß!")
+                    display_message = bot.sendMessage(chat_id, "Hauptmenü", reply_markup=json.dumps(menüs["haupt"]))
+                else:
+                    display_message = bot.sendMessage(chat_id, "Bitte gibt das korrekte Passwort ein. Wenn du nicht weiter weißt, wende dich an " + SUPPORTTEAM + ".")
+
+            elif modus == M_NORMAL:
+                # Hier nach tags für Gruppennachrichten suchen
+                print("Los gehts!")
+
+            else:
+                print("MODUS-FAIL!")
+                print(modus)
+
+        # debugging
+        pp.pprint(users)
+
 
     elif flavor == "callback_query":
         button = msg["data"]
         msg_id = msg["message"]["message_id"]
         chat_id = msg["message"]["chat"]["id"]
-
-        print("Nächstes Menü: " + button)
+        callback_id = msg["id"]
 
         if button == "haupt":
+            bot.answerCallbackQuery(callback_id)
+
             display_message = bot.editMessageText((chat_id, msg_id), "Hauptmenü", reply_markup=json.dumps(menüs["haupt"]))
 
+        elif button == "tür":
+            if data["tür"]:
+                open_text = "Die Tür ist offen. Hier kannst du sie schließen." # Das später zufällig
+
+                bot.answerCallbackQuery(callback_id)
+                display_message = bot.editMessageText((chat_id,msg_id), open_text, reply_markup=json.dumps(menüs["türzu"]))
+            else:
+                closed_text = "Die Tür ist zu. Hier kannst du sie öffnen."
+
+                bot.answerCallbackQuery(callback_id)
+                display_message = bot.editMessageText((chat_id, msg_id), closed_text, reply_markup=json.dumps(menüs["türoffen"]))
+
         elif button == "türzu":
-            display_message = bot.editMessageText((chat_id,msg_id), "Türzumenü", reply_markup=json.dumps(menüs["türzu"]))
+            closing_text = "Die Tür ist jetzt zu"
+
+            data["tür"] = False
+            save("Daten/data.json", data)
+
+            bot.answerCallbackQuery(callback_id, text=closing_text, show_alert=True)
+            display_message = bot.sendMessage(chat_id, "Hauptmenü", reply_markup=json.dumps(menüs["haupt"]))
 
         elif button == "türoffen":
-            display_message = bot.editMessageText((chat_id, msg_id), "Türoffenmenü", reply_markup=json.dumps(menüs["türoffen"]))
+            opening_text = "Die Tür ist jetzt offen"
+
+            data["tür"] = True
+            save("Daten/data.json", data)
+
+            bot.answerCallbackQuery(callback_id, text=opening_text, show_alert=True)
+            display_message = bot.sendMessage(chat_id, "Hauptmenü", reply_markup=json.dumps(menüs["haupt"]))
 
         elif button == "funktionen":
             display_message = bot.editMessageText((chat_id, msg_id), "Funktionsmenü", reply_markup=json.dumps(menüs["funktionen"]))
@@ -95,7 +232,7 @@ def handle(msg):
 # ---   Telepot Message Loop starten    --- #
 #############################################
 
-bot = telepot.Bot("698839060:AAEPazfjo1gvbO-DFxi6cbpq-rAOIVJPJ2g")
+bot = telepot.Bot(TOKEN)
 
 MessageLoop(bot, handle).run_as_thread()
 print("Ich lese mit ...")
